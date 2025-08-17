90b428eadf69017cd2c337709743636caca08109
arngonzale-eng <arn.gonzale@gmail.com>
2025-08-17 00:01:05 -0500

    Update streamlit_app.py
    Otro lab

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """)
 
-
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
@@ -32,17 +30,20 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
     
     #st.write(ingredients_string)
-
     
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
 
     #st.write(my_insert_stmt)
     #st.stop
-    
     time_to_insert = st.button ('Submit Order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
